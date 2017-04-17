80893a55752541933ce554e6f645c255906d2c52
whamsy <waatluri@gmail.com>
2017-04-17 19:20:03 -0400

    Completed SNS/SQS implementation

--- PracticeScripts/consumer.js
+++ PracticeScripts/consumer.js
@@ -3,17 +3,20 @@
  */
 
 var Consumer = require('sqs-consumer');
+// var Publisher = require('aws-sns-publish');
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-west-2'});
 
 var MonkeyLearn = require('monkeylearn');
 var ml = new MonkeyLearn('f42620f327e7f9d3f76ddc93654e594b94eae8e6');
 var module_id = 'cl_qkjxv9Ly';
 
-// var text_list = ["This is a text to test your classifier", "This is some more text"];
-
 
 var app = Consumer.create({
     queueUrl: 'https://sqs.us-west-2.amazonaws.com/453367379586/CloudTweetMap',
     handleMessage: (message, done) => {
+
+        var Publisher = require('aws-sns-publish');
 
         var data = JSON.parse(JSON.parse(message.Body).Message);
 
@@ -35,9 +38,29 @@ var app = Consumer.create({
                     probability:res.result[0][0]['probability']
                 }
             };
-            // console.log(res.result[0][0]['label']);
-            console.log(tweetdata);
+
+            var senti_sns_output = JSON.stringify(tweetdata);
+            var subject_txt = "Processed Tweet "+tweetdata['id'];
+
+            console.log("Input to 2nd Queue is of type ",typeof senti_sns_output)
+
+            Publisher(senti_sns_output, {arn: 'arn:aws:sns:us-west-2:453367379586:SentiTweet'});
+            
         });
+
+        done();
+
+    }
+});
+
+var app2 = Consumer.create({
+    queueUrl: 'https://sqs.us-west-2.amazonaws.com/453367379586/SentiTweetQueue',
+    handleMessage: (message, done) => {
+
+        var final_data = JSON.parse(JSON.parse(message.Body).Message);
+
+        console.log("2nd Queue output is of type ", typeof final_data);
+
 
         done();
 
@@ -49,3 +72,9 @@ app.on('error', function (err) {
 });
 
 app.start();
+
+app2.on('error', function (err) {
+    console.log(err);
+});
+
+app2.start();
